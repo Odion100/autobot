9bfc589a35cb2557de0b1a676279696242eadeaf
odion100 <odionedwards@gmail.com>
2024-09-06 11:27:09 -0400

    feat(agents): updating WebAssistant agent prompt

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import WebAssistant from "./agents/WebAssistant/index.js";
 import { deleteScreenshots } from "./common/utils/index.js";
 import ElementIdentifier from "./modules/ElementIdentifier.js";
 import Agentci from "agentci";
-import fs from "fs";
 
-const state = { messages: [] };
 driver.init({
   ElementIdentifier: Agentci().rootAgent(ElementIdentifier),
   WebAssistant,
